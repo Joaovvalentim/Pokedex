3650f3a8ebec17d98b96237b6be394ea8a4e97fc
Joaovvalentim <joao.vcitor.valentim@hotmail.com>
2021-07-29 12:37:53 -0300

    Second Commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function renderPokeDetails() {
     return (
         `
     <h1>${selectedPokemon.name}.  Nº${selectedPokemon.id}</h1>
-    <div class="img">
+    <div  class="img ">
         <img class="selectImage" src="https://pokeres.bastionbot.org/images/pokemon/${selectedPokemon.id}.png" />
         <div class="status">
             <div class="columUm">
@@ -35,7 +35,7 @@ function renderPokeDetails() {
                 </li>
                 <li>
                      <span class="atributeTitle">Peso: </span>
-                      <span class="atributeValue">${selectedPokemon.weight / 10} Kg.</span>
+                    <span class="atributeValue">${selectedPokemon.weight / 10} Kg.</span>
                 </li>
             </ul>
             </div>
@@ -50,6 +50,13 @@ function renderPokeDetails() {
             </div>
         </div>
     </div>
+        <div class="moves">
+                 <ul class="colum">
+                     <h1 class="atributeValue">Moves: </h1>
+                     ${renderMoves()}
+                 </ul>   
+
+         </div>
     `
     )
 }
@@ -80,6 +87,19 @@ function renderTypes() {
     })
     return acumulator.join('');
 }
+function renderMoves() {
+    const acumulator = [];
+    selectedPokemon.moves.forEach(attribute => {
+        acumulator.push(
+            `
+            <li class="attrLine">
+                <span class="atributeTitle">${attribute.move.name}:</span>
+            </li>
+            `
+        )
+    })
+    return acumulator.join('');
+}
 
 //gerar e retornar o array de promises de pokemons
 const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
@@ -89,13 +109,17 @@ const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
 const generateHTML = pokemons => pokemons.reduce((accumulator, pokemon) => {
     const elementTypes = pokemon.types.map(typeInfo => typeInfo.type.name)
     accumulator += `
-                <li onClick="openPokemon(${pokemon.id})" class="card ${elementTypes[0]}">
-                    <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
-                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                    <p class"card-subtitle">${elementTypes.join(' | ')}<p>
+                <li onclick="openPokemon(${pokemon.id})" class="card ${elementTypes[0]}">
+                    <div>
+                        <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+                        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                        <p class"card-subtitle">${elementTypes.join(' | ')}<p>
+                    </div>
                 </li>`
     return accumulator
-}, '')
+}, '');
+
+
 
 // gera todos os dados pegos na API na tela em formato HTML
 const insertPokemonsInToPage = pokemons => {
